690959105a951406dd58ce073b4ab8c867f49ccd
McPsalmy <mcpsalmy@gmail.com>
2026-02-17 22:38:20 +0000

    track done

--- app/api/public/tracks/route.ts
+++ app/api/public/tracks/route.ts
@@ -1,6 +1,4 @@
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
 
 import { NextResponse } from "next/server";
 import { supabaseClient } from "../../../lib/supabaseClient";
